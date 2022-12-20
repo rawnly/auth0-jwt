ada98189359c4ea2d32269b6172343c4f4aed8e1
Federico Vitale <fedevitale99@gmail.com>
2022-12-20 01:25:41 +0100

    chore(axum-example): updated with real example

--- examples/axum-hello-world/src/main.rs
+++ examples/axum-hello-world/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use auth0_jwt::claims::Claims;
+use axum::{response::IntoResponse, routing::get, Json, Router};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// just use the given extractor
+async fn root(Claims(claims): Claims<ClaimsContent>) -> impl IntoResponse {
+    Json(claims)
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct ClaimsContent {
+    iat: usize,
+    exp: usize,
+    aud: String,
 }
